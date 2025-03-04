6e0b4edf9a06e3c9b60ea51322ad535cd1a7e336
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-04 10:45:04 +0530

    Initial commit

---
+++ src/test/java/my/mood/UserManagementSystem/User_Management_System/UserManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package my.mood.UserManagementSystem.User_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
