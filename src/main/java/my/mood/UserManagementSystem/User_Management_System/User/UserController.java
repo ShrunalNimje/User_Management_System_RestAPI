55c050e82211f3b0744bddd69418bbf03cf099d9
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-06 23:52:39 +0530

    replace services with UserRepository interface

--- src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserController.java
+++ src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserController.java
@@ -1,7 +1,9 @@
 package my.mood.UserManagementSystem.User_Management_System.User;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,38 +12,65 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class UserController {
 
-	UserService service;
+//	UserService service;
 	
-	public UserController(UserService service) {
-		this.service = service;
+	@Autowired
+	UserRepository repository;
+	
+	public UserController(/*UserService service, */ UserRepository repository) {
+//		this.service = service;
+		this.repository = repository;
 	}
 	
 	@GetMapping("/users")
 	public List<User_Entity> retrieveUsers() {
-		return service.retrieveAllUser();
+//		return service.retrieveAllUser();
+		return repository.findAll();
 	}
 	
 	@GetMapping("/users/{id}")
-	public User_Entity retrieveSpecificUser(@PathVariable int id) {
-		return service.retrieveSpecificUser(id);
+	public Optional<User_Entity> retrieveSpecificUser(@PathVariable int id) {
+//		return service.retrieveSpecificUser(id);
+		return repository.findById(id);
 	}
 	
 	@DeleteMapping("/users/{id}")
-	public void deleteUser(@PathVariable int id) {
-		service.deleteSpecificUser(id);
+	public void deleteUser(@Valid @PathVariable int id) {
+//		service.deleteSpecificUser(id);
+		repository.deleteById(id);
 	}
 	
 	@PostMapping("/users")
-	public void addUser(@RequestBody User_Entity user) {
-		service.addSpecificUser(user);
+	public void addUser(@Valid @RequestBody User_Entity user) {
+//		service.addSpecificUser(user);
+		repository.save(user);
 	}
 	
 	@PutMapping("/users/{id}") 
-	public void updateUser(@RequestBody User_Entity user, @PathVariable int id) {
-		service.updateSpecificUser(id, user);
+	public User_Entity updateUser(@Valid @RequestBody User_Entity user, @Valid @PathVariable int id) {
+//		service.updateSpecificUser(id, user);
+		Optional<User_Entity> existingUser = retrieveSpecificUser(id);
+		
+		if(existingUser.isPresent()) {
+			User_Entity tempUser = existingUser.get();
+			
+			tempUser.setEmail(user.getEmail());
+			tempUser.setName(user.getName());
+			tempUser.setPassword(user.getPassword());
+			tempUser.setRole(user.getRole());
+			
+			tempUser.setId(id);
+			
+			return repository.save(tempUser);
+		}
+		else {
+			throw new RuntimeException("User not found with id = " + id);
+		}
 	}
 	
 }
