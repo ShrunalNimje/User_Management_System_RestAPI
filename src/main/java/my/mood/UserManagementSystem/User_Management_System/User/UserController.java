34742cd3ca706da9bf12b28f21b4b25984604b68
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-10 22:35:13 +0530

    user registration

--- src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserController.java
+++ src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserController.java
@@ -3,7 +3,8 @@ package my.mood.UserManagementSystem.User_Management_System.User;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,15 +17,13 @@ import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
-
-//	UserService service;
 	
-	@Autowired
 	UserRepository repository;
+	PasswordEncoder encoder;
 	
-	public UserController(/*UserService service, */ UserRepository repository) {
-//		this.service = service;
+	public UserController(UserRepository repository, PasswordEncoder encoder) {
 		this.repository = repository;
+		this.encoder = encoder;
 	}
 	
 	@GetMapping("/users")
@@ -72,5 +71,31 @@ public class UserController {
 			throw new RuntimeException("User not found with id = " + id);
 		}
 	}
+	
+	@PostMapping("/users/register")
+	public ResponseEntity<String> registerUser(@RequestBody UserRegistrationDTO userDTO) {
+		Optional<User_Entity> existingUser = repository.findByName(userDTO.getName());
+		
+		if(existingUser.isPresent()) {
+			return ResponseEntity.badRequest().body("Username already exists!");
+		}
+		
+		User_Entity users = new User_Entity();
+		
+		users.setName(userDTO.getName());
+		users.setEmail(userDTO.getEmail());
+		users.setPassword(encoder.encode(userDTO.getPassword()));
+		users.setRole(User_Role.valueOf(userDTO.getRole().toUpperCase()));
+		
+		repository.save(users);
+		
+		return ResponseEntity.ok("User registered successfully with role: " + users.getRole());
+	}
+	
+	@GetMapping("/users/profile")
+	public String userProfile() {
+		return "Welcome to User Profile!";
+	}
+	
 	
 }
