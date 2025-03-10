97ae21cb4355042b17dd66f76105c1acc991e684
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-10 22:32:38 +0530

    custom methods in user repository

--- src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserRepository.java
+++ src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserRepository.java
@@ -1,9 +1,13 @@
 package my.mood.UserManagementSystem.User_Management_System.User;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User_Entity, Integer>{
-	
+
+	Optional<User_Entity> findByEmail(String email);
+	Optional<User_Entity> findByName(String name);
 }
