7c94926c14e6cdbe62fb8aa08ae338344650614e
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-12 23:09:10 +0530

    added user can update and delete their own account

--- src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserController.java
+++ src/main/java/my/mood/UserManagementSystem/User_Management_System/User/UserController.java
@@ -1,7 +1,13 @@
 package my.mood.UserManagementSystem.User_Management_System.User;
 
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +21,35 @@ public class UserController {
 		this.encoder = encoder;
 	}
 	
-	@GetMapping("/user/profile")
+	@DeleteMapping("/user/delete/")
+	public ResponseEntity<?> deleteUser(Principal principal) {
+		User_Entity existingUser = repository.findByEmail(principal.getName())
+				.orElseThrow(() -> new RuntimeException("User not found!"));
+		
+		repository.delete(existingUser);
+		
+		return ResponseEntity.ok("User account deleted successfully!");
+	}
+	
+	@PutMapping("/user/update/")
+	public ResponseEntity<?> updateUser(Principal principal, @RequestBody User_Entity updatedUser) {
+		User_Entity existingUser = repository.findByEmail(principal.getName())
+				.orElseThrow(() -> new RuntimeException("User not found!"));
+		
+		if(updatedUser.getName() != null) {
+			existingUser.setName(updatedUser.getName());
+		}
+		
+		if(updatedUser.getPassword() != null) {
+			existingUser.setPassword(encoder.encode(updatedUser.getPassword()));
+		}
+		
+		repository.save(existingUser);
+		
+		return ResponseEntity.ok("User account updated successfully!");
+	}
+	
+	@GetMapping("/user/profile/")
 	public String userProfile() {
 		return "Welcome to User Profile!";
 	}
