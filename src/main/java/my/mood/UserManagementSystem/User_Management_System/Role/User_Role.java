25ebc982abe754aecfd9b98c21420ec052a9dd65
Shrunal Nimje <shrunalnimje@gmail.com>
2025-04-16 10:14:50 +0530

    moved repo layer & user role to repo & role package

---
+++ src/main/java/my/mood/UserManagementSystem/User_Management_System/Role/User_Role.java
@@ -0,0 +1,6 @@
+package my.mood.UserManagementSystem.User_Management_System.Role;
+
+public enum User_Role {
+	ROLE_USER,
+	ROLE_ADMIN
+}
--- src/main/java/my/mood/UserManagementSystem/User_Management_System/User/User_Role.java
+++
@@ -1,6 +0,0 @@
-package my.mood.UserManagementSystem.User_Management_System.User;
-
-public enum User_Role {
-	ROLE_USER,
-	ROLE_ADMIN
-}
